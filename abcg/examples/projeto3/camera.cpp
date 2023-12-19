bfd66cbc0f6330ec727f656da685ecffc2e1c88d
tunin <pedrotunin@gmail.com>
2023-12-18 22:05:04 -0300

    feat: base

---
+++ abcg/examples/projeto3/camera.cpp
@@ -0,0 +1,61 @@
+#include "camera.hpp"
+
+#include <glm/gtc/matrix_transform.hpp>
+
+void Camera::computeProjectionMatrix(glm::vec2 const &size) {
+  m_projMatrix = glm::mat4(1.0f);
+  auto const aspect{size.x / size.y};
+  m_projMatrix = glm::perspective(glm::radians(70.0f), aspect, 0.1f, 25.0f);
+}
+
+void Camera::computeViewMatrix() {
+  m_viewMatrix = glm::lookAt(m_eye, m_at, m_up);
+}
+
+void Camera::climb(float speed) {
+  // Compute up vector
+  auto const up{glm::normalize(m_up)};
+
+  // Move eye and center up (speed > 0) or down (speed < 0)
+  m_eye += up * speed;
+  m_at += up * speed;
+
+  computeViewMatrix();
+}
+
+void Camera::dolly(float speed) {
+  // Compute forward vector (view direction)
+  auto const forward{glm::normalize(m_at - m_eye)};
+
+  // Move eye and center forward (speed > 0) or backward (speed < 0)
+  m_eye += forward * speed;
+  m_at += forward * speed;
+
+  computeViewMatrix();
+}
+
+void Camera::truck(float speed) {
+  // Compute forward vector (view direction)
+  auto const forward{glm::normalize(m_at - m_eye)};
+  // Compute vector to the left
+  auto const left{glm::cross(m_up, forward)};
+
+  // Move eye and center to the left (speed < 0) or to the right (speed > 0)
+  m_at -= left * speed;
+  m_eye -= left * speed;
+
+  computeViewMatrix();
+}
+
+void Camera::pan(float speed) {
+  glm::mat4 transform{1.0f};
+
+  // Rotate camera around its local y axis
+  transform = glm::translate(transform, m_eye);
+  transform = glm::rotate(transform, -speed, m_up);
+  transform = glm::translate(transform, -m_eye);
+
+  m_at = transform * glm::vec4(m_at, 1.0f);
+
+  computeViewMatrix();
+}
